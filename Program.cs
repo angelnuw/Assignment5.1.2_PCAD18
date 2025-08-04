eded1886539b36e87987a52b099ddd93a7fc9dd9
Angel Nuw <Angel.Nuw@mssa.newhorizons.com>
2025-08-04 16:55:49 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Assignment5._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            int sum = 0;
+            
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Enter number {0}:", i + 1);
+                number = Convert.ToInt32(Console.ReadLine());
+                sum += number;
+            }
+
+            Console.WriteLine("The sum of the four numbers is: " + sum);
+        }
+    }
+}
